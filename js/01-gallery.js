7257226ae1e9862c8baca8a076d71f69391fc586
Vaskovskiii <rostikfutu4e250303@ukr.net>
2022-06-03 18:49:33 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,36 +1,36 @@
 import { galleryItems } from './gallery-items.js'
-
-const instance = basicLightbox.create(`
-    <img src="" width="800" height="600">
-`)
-instance.show()
-
 // Change code below this line
+//import img
 console.log(galleryItems)
-
 const refs = {
   gallery: document.querySelector('.gallery'),
-  image: document.createElement('img'),
-  href: document.querySelector('a'),
-  lightbox: document.querySelector('.lightbox'),
 }
 
 const createGalleryItem = ({ preview, original, description }) =>
-  `<div class="gallery__item"><a class="gallery__link" href=${original}><img class="gallery__image" src=${preview} data-source=${original} alt=${description}/></a></div>`
-
+  `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>
+    `
 const galleryMarkup = galleryItems.reduce(
   (acc, item) => acc + createGalleryItem(item),
   '',
 )
 refs.gallery.insertAdjacentHTML('afterbegin', galleryMarkup)
+refs.gallery.addEventListener('click', getImg)
+function getImg(event) {
+  event.preventDefault()
+  if (event.target.nodeName === 'IMG') {
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    `)
 
-refs.gallery.addEventListener('click', onGalleryClick)
-
-function onGalleryClick(e) {
-  e.preventDefault()
-  if (e.target.nodeName !== 'IMG') {
-    return
+    instance.show()
   }
-  instance.element().querySelector('img').src = e.target.dataset.source
-  instance.show()
 }
