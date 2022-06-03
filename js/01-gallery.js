64c36078cb32cdd7337106374e1e1091b51be97e
Vaskovskiii <rostikfutu4e250303@ukr.net>
2022-06-03 12:14:55 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,36 @@
 import { galleryItems } from './gallery-items.js'
-// Change code below this line
 
+const instance = basicLightbox.create(`
+    <img src="" width="800" height="600">
+`)
+instance.show()
+
+// Change code below this line
 console.log(galleryItems)
 
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  image: document.createElement('img'),
+  href: document.querySelector('a'),
+  lightbox: document.querySelector('.lightbox'),
+}
+
+const createGalleryItem = ({ preview, original, description }) =>
+  `<div class="gallery__item"><a class="gallery__link" href=${original}><img class="gallery__image" src=${preview} data-source=${original} alt=${description}/></a></div>`
+
+const galleryMarkup = galleryItems.reduce(
+  (acc, item) => acc + createGalleryItem(item),
+  '',
+)
+refs.gallery.insertAdjacentHTML('afterbegin', galleryMarkup)
+
+refs.gallery.addEventListener('click', onGalleryClick)
+
+function onGalleryClick(e) {
+  e.preventDefault()
+  if (e.target.nodeName !== 'IMG') {
+    return
+  }
+  instance.element().querySelector('img').src = e.target.dataset.source
+  instance.show()
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js'
 // Change code below this line
 
 console.log(galleryItems)
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  image: document.createElement('img'),
+  href: document.querySelector('a'),
+  lightbox: document.querySelector('.lightbox'),
+}
+
+const createGalleryItem = ({ preview, original, description }) =>
+  `<a class="gallery__item" href=${original}>
+    <img class="gallery__image" src=${preview} alt=${description} />
+  </a>`
+const galleryMarkup = galleryItems.reduce(
+  (acc, item) => acc + createGalleryItem(item),
+  '',
+)
+refs.gallery.insertAdjacentHTML('afterbegin', galleryMarkup)
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  animationSpeed: 250,
+  navText: ['&#8249;', '&#8250;'],
+})
+gallery.on('show.simplelightbox', function () {})
